60b5fbd9d525c58a476b2434462e50bd2fcff967
jspmern <jspmern@gmail.com>
2025-11-30 14:43:47 +0530

    check all setting and structure output by prompt

--- app.js
+++ app.js
@@ -4,17 +4,27 @@ import dotenv from "dotenv";
 const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 const main=async ()=>{
    const response= await getInvokeGroq();
-   console.log(response.choices[0].message);
+   console.log(response.choices[0].message.content);
 }
 export const getInvokeGroq= async()=>{
     return groq.chat.completions.create({
     messages:[
+       {
+            role:'system',
+            content:`Your name is Utsav prsonal bot assistant. and known as jhaji. You are friendly and always ready to help.  and your are expert in web development. and give answer in text format only. you  must return in valid json structure only.
+            example:{
+                'answer':{string}
+            }`
+       } ,
         {
             role:'user',
-            content:'hi'
+            content:'what is react?'
         }
     ],
     model: "llama-3.1-8b-instant",
+    // temperature: 0.7,
+    // max_completion_tokens:1,
+    // frequency_penalty:0,
   });
 }
 main();
